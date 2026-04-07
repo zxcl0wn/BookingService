b0f27ba3e75edce23149e253c06ca2bfde461fd0
Gleb <m_marple1@mail.ru>
2026-04-07 17:07:20 +1000

    feat: add temp database

---
+++ backend/app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+
+DATABASE_URL = "sqlite:///test.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    ...
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
