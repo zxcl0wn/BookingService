3b431432e60c8bf54a9a1928768b68e3d56df8a8
Gleb <m_marple1@mail.ru>
2026-04-10 22:45:32 +1000

    feat: add hashed password in db

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from ..models import User
+from ..auth.security import verify_password, get_password_hash
 
 
 class UserRepository:
@@ -13,7 +14,14 @@ class UserRepository:
         return self.db.get(User, users_id)
 
     def create(self, user: dict) -> User:
-        new_user = User(**user)
+        hashed_password = get_password_hash(user["password"])
+        new_user = User(
+            username=user["username"],
+            email=user["email"],
+            name=user["name"],
+            phone=user["phone"],
+            password=hashed_password
+        )
         self.db.add(new_user)
         self.db.commit()
         self.db.refresh(new_user)
@@ -21,10 +29,14 @@ class UserRepository:
 
     def update(self, user_id: int, user_data: dict) -> User|None:
         user = self.db.get(User, user_id)
+        hashed_password = get_password_hash(user_data["password"])
         if user:
             for key, value in user_data.items():
                 if value is not None:
-                    setattr(user, key, value)
+                    if key is "password":
+                        setattr(user, key, hashed_password)
+                    else:
+                        setattr(user, key, value)
             self.db.commit()
             self.db.refresh(user)
             return user
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -2,31 +2,46 @@ from fastapi import HTTPException, status
 from ..repositories import UserRepository
 from sqlalchemy.orm import Session
 from ..schemas.user_schema import UserResponse, UserCreate, UserUpdate
+from sqlalchemy.exc import IntegrityError
 
 
 class UserService:
     def __init__(self, db: Session):
         self.db = UserRepository(db)
 
+
     def get_all(self) -> list[UserResponse]:
         users = self.db.get_all()
         return [UserResponse.model_validate(user) for user in users]
 
+
     def get_by_id(self, user_id: int) -> UserResponse:
+        user = self.db.get_by_id(user_id)
+        print(f'{user = }')
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        return UserResponse.model_validate(user)
+
+
+    def create(self, user: UserCreate) -> UserResponse:
+        try:
+            new_user = self.db.create(user.model_dump())
+            return UserResponse.model_validate(new_user)
+        except IntegrityError:
+            raise HTTPException(status_code=409, detail="User already exists")
+
+
+    def update(self, user_id: int, user_data: UserUpdate) -> UserResponse:
         user = self.db.get_by_id(user_id)
         if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        return UserResponse.model_validate(user)
 
-    def create(self, user: UserCreate) -> UserResponse:  # TODO: hashed password
-        new_user = self.db.create(user.model_dump())
-        return UserResponse.model_validate(new_user)
+        try:
+            updated_user = self.db.update(user_id, user_data.model_dump())
+            return UserResponse.model_validate(updated_user)
+        except:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to update user")
 
-    def update(self, user_id: int, user_data: UserUpdate) -> UserResponse:
-        user = self.db.update(user_id, user_data.model_dump())
-        if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        return UserResponse.model_validate(user)
 
     def delete(self, user_id: int) -> UserResponse:
         user = self.db.delete(user_id)
